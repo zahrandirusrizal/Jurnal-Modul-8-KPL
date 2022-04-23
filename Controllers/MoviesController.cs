e25c9cf2f362b0112596e13d4db53ed69cf5f592
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-04-23 11:13:44 +0700

    Menambahkan kelas controller movies

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace modul8_1302204080.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private static List<Movies> listFilm = new List<Movies>()
+        {
+            new Movies("The Shawshank Redemption", "Frank Darabont", "Morgan Freeman", "Tim Robbins", "Bob Gunton" , "Wlliam Sadler" , "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."),
+            new Movies("The Godfather", "Francis Ford Coppola", "Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton", "The aging patriarch of an organized crime dynasty in postwar New York City transfers control of his clandestine empire to his reluctant youngest son."),
+            new Movies("The Dark Knight", "Christopher Nolan", "Christian Bale", "Heath Ledger", "Aaron Eckhart", "Michael Caine", "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.")
+        };
+        
+
+
+        // GET: api/Movies
+        [HttpGet]
+        public IEnumerable<Movies> Get()
+        {
+            return listFilm;
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public Movies Get(int id)
+        {
+            return listFilm[id];
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] Movies value)
+        {
+            listFilm.Add(value);
+        }
+
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            listFilm.RemoveAt(id);
+        }
+    }
+}
---
+++ Movies.cs
@@ -0,0 +1,23 @@
+﻿namespace modul8_1302204080
+{
+    public class Movies
+    {
+        public string title {get; set; }
+        public string director { get; set; }
+        public List<string> Stars { get; set; }
+        public string description { get; set; }
+
+        public Movies(string judul, string direktor, string star1, string star2, string star3, string star4, string deskripsi)
+        {
+            this.title = judul;
+            this.director = direktor;
+            this.Stars = new List<string>();
+            this.Stars.Add(star1);
+            this.Stars.Add(star2);
+            this.Stars.Add(star3);
+            this.Stars.Add(star4);
+            this.description = deskripsi;
+        }
+
+    }
+}
