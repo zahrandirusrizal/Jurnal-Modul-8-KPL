c023742b3cc6538246892cc93ee1c41c4b622a72
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-04-23 10:06:51 +0700

    Add project files.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace modul8_1302204080
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
